7b1724b4b2ee7ae13c52dfa60e88d3ae34f0c951
Lucien George <luciengeorge95@gmail.com>
2019-02-25 22:58:31 +0000

    bug fix on new form

--- src/containers/posts_new.jsx
+++ src/containers/posts_new.jsx
@@ -11,33 +11,18 @@ class PostsNew extends Component {
     });
   }
 
-  renderField(field) {
-    return (
-      <div className="form-group">
-        <label>{field.label}</label>
-        <input className="form-control" type={field.type}/>
-      </div>
-    );
-  }
-
   render() {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
-          <Field
-            label="Title"
-            name="title"
-            type="text"
-            component={this.renderField}
-          />
-          <label htmlFor="content">Content</label>
-          <Field
-            className="form-control"
-            label="Content"
-            name="content"
-            component="textarea"
-            rows="8"
-          />
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <Field label="Title" name="title" type="text" component="input" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="content">Content</label>
+            <Field className="form-control" label="Content" name="content" component="textarea" rows="8" />
+          </div>
           <button className="btn btn-primary" type="submit" disabled={this.props.pristine || this.props.submitting}>
             Create Post
           </button>
